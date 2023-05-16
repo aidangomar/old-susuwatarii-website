c838e700e0c399fd81c095d52b425d7d2e89b044
Aidan <aidangomar@gmail.com>
2023-05-15 18:27:26 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Body from './components/Body';
+import Head from './components/Head';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Head/>
+      <Body/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
